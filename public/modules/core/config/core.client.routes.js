5d28eeede1fe49590493e40686637a0db17568c0
Dave Seidman <david.seidman@duke.edu>
2015-07-15 11:41:24 -0400

    Working test view under core views

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -12,5 +12,12 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
 		});
+
+		// Home state routing
+		$stateProvider.
+			state('test', {
+				url: '/test',
+				templateUrl: 'modules/core/views/test.html'
+			});
 	}
 ]);
