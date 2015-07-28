7280b09a000cf9622545caf0948d8c3b74da8255
Dave Seidman <david.seidman@duke.edu>
2015-07-28 10:42:59 -0400

    Getting some data for positions route

--- public/modules/positions/services/positions.client.service.js
+++ public/modules/positions/services/positions.client.service.js
@@ -1,9 +1,9 @@
 'use strict';
 
-//Articles service used for communicating with the articles REST endpoints
-angular.module('articles').factory('Articles', ['$resource', function($resource) {
-    return $resource('articles/:articleId', {
-        articleId: '@_id'
+//Positions service used for communicating with the positions REST endpoints
+angular.module('positions').factory('Positions', ['$resource', function($resource) {
+    return $resource('positions/:positionId', {
+        positionId: '@_id'
     }, {
         update: {
             method: 'PUT'
