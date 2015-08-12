ecaa3cda215cdda8f23a759a796c2fe07fe18179
shotokai <david.parrish@duke.edu>
2015-08-12 14:22:49 -0400

    - added sidear menu items from Users - changing handling of 403 interceptor to redirect to 'home', adding message with error.

--- public/modules/users/config/users.client.config.js
+++ public/modules/users/config/users.client.config.js
@@ -1,30 +1,38 @@
 'use strict';
 
 // Config HTTP Error Handling
-angular.module('users').config(['$httpProvider',
-	function($httpProvider) {
-		// Set the httpProvider "not authorized" interceptor
-		$httpProvider.interceptors.push(['$q', '$location', 'Authentication',
-			function($q, $location, Authentication) {
-				return {
-					responseError: function(rejection) {
-						switch (rejection.status) {
-							case 401:
-								// Deauthenticate the global user
-								Authentication.user = null;
+angular.module('users')
+  .config(['$httpProvider',
+    function ($httpProvider) {
+      // Set the httpProvider "not authorized" interceptor
+      $httpProvider.interceptors.push(['$q', '$location', 'Authentication', 'Messages',
+        function ($q, $location, Authentication, Messages) {
+          return {
+            responseError: function (rejection) {
+              switch (rejection.status) {
+                case 401:
+                  // Deauthenticate the global user
+                  Authentication.user = null;
+                  // Redirect to signin page
+                  $location.path('signin');
+                  break;
+                case 403:
+                  Messages.addMessage('You are not authorized to access the underlying data. Details: Url Requested: "' + rejection.config.url + '"" method: "' + rejection.config.method + '"', 'warn');
+                  $location.path('home');
 
-								// Redirect to signin page
-								$location.path('signin');
-								break;
-							case 403:
-								// Add unauthorized behaviour 
-								break;
-						}
-
-						return $q.reject(rejection);
-					}
-				};
-			}
-		]);
-	}
-]);
+                  // Add unauthorized behaviour
+                  break;
+              }
+              return $q.reject(rejection);
+            }
+          };
+        }
+      ]);
+    }
+  ])
+  .run(['Menus',
+    function (Menus) {
+      // Set sidebar menu items
+      Menus.addMenuItem('sidebar', 'Users', 'users/list');
+      Menus.addMenuItem('sidebar', 'Add User', 'users/create');
+    }]);
