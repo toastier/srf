0aa2a744e883c29125008575c336bc378635316c
shotokai <david.parrish@duke.edu>
2015-08-12 14:08:47 -0400

    - users.server.controller.js   - added methods adminCreate, roles, adminUpdate, list - users.server.routes.js   - added POST and GET routes for 'users'   - modified PUT route for 'users'   - added PUT route for 'users/me/update   - added authorization to admin routes

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -5,39 +5,44 @@
  */
 var passport = require('passport');
 
-module.exports = function(app) {
-	// User Routes
-	var users = require('../../app/controllers/users');
-	app.route('/users/me').get(users.me);
-	app.route('/users').put(users.update);
-	app.route('/users/password').post(users.changePassword);
-	app.route('/users/accounts').delete(users.removeOAuthProvider);
+module.exports = function (app) {
+  // User Routes
+  var users = require('../../app/controllers/users');
+  app.route('/users/me').get(users.me);
+  app.route('/users/me/update').get(users.update);
+  app.route('/users')
+    .put(users.requiresLogin, users.hasAuthorization(['admin']), users.adminUpdate)
+    .post(users.requiresLogin, users.hasAuthorization(['admin']), users.adminCreate)
+    .get(users.requiresLogin, users.hasAuthorization(['admin']), users.list);
+  app.route('/users/password').post(users.changePassword);
+  app.route('/users/accounts').delete(users.removeOAuthProvider);
+  app.route('/roles').get(users.requiresLogin, users.hasAuthorization(['admin']), users.roles);
 
-	// Setting up the users api
-	app.route('/auth/signin').post(users.signin);
-	app.route('/auth/signout').get(users.signout);
+  // Setting up the users api
+  app.route('/auth/signin').post(users.signin);
+  app.route('/auth/signout').get(users.signout);
 
-	// Setting the Saml routes
-	app.route('/auth/saml').get(
-		passport.authenticate('saml',
-			{
-				successRedirect: '/',
-				failureRedirect: 'signin'
-			}),
-		function(req, res) {
-			res.redirect('/');
-		});
+  // Setting the Saml routes
+  app.route('/auth/saml').get(
+    passport.authenticate('saml',
+      {
+        successRedirect: '/',
+        failureRedirect: 'signin'
+      }),
+    function (req, res) {
+      res.redirect('/');
+    });
 
-	app.route('/auth/saml/callback').post(
-		passport.authenticate('saml',
-			{
-				failureRedirect: '/',
-				failureFlash: true
-			}),
-		function(req, res) {
-			res.redirect('/');
-		});
+  app.route('/auth/saml/callback').post(
+    passport.authenticate('saml',
+      {
+        failureRedirect: '/',
+        failureFlash: true
+      }),
+    function (req, res) {
+      res.redirect('/');
+    });
 
-	// Finish by binding the user middleware
-	app.param('userId', users.userByID);
+  // Finish by binding the user middleware
+  app.param('userId', users.userByID);
 };
