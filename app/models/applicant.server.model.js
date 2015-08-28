da4fa0dc4fafcb38919693cf7d8ae816ffdc350a
Dave Seidman <david.seidman@duke.edu>
2015-08-28 12:34:00 -0400

    Applicant model updated

--- app/models/applicant.server.model.js
+++ app/models/applicant.server.model.js
@@ -20,24 +20,81 @@ var applicantModel = new Schema({
         positionAppliedFor: {
             positionName:   { type: String}
         },
-    //TODO rename this
-        applicantPositions: [{
+        currentPositions: [{
             positionName:           { type: String },
             dateExpectedCompletion: { type: Date },
             note:                   { type: String },
-            //TODO unnecessary layer; fix
-            positions: [{
-                //TODO institutions will lookup in another schema
-                institutions: [{
-                    institutionName: { type: String },
-                    city: { type: String },
-                    state: { type: String },
-                    country: { type: String }
-                }]
+            //TODO institutions will lookup in another schema
+            institution: [{
+                institutionName: { type: String },
+                city: { type: String },
+                state: { type: String },
+                country: { type: String }
             }]
         }],
+        focalAreas: [{
+            focalArea: { type: String }
+        }],
+        resume: [{
+            dateUploaded:   { type: Date },
+            link:           { type: String },
+            note:           { type: String }
+        }],
+        emailAddresses: [{
+            emailAddress:   { type: String },
+            primary:        { type: Boolean },
+            note:           { type: String }
+        }],
+        phoneNumbers: [{
+            phoneNumber:    { type: String },
+            type:           { type: String }, //TODO enumlist
+            note:           { type: String },
+            primary:        { type: Boolean },
+        }],
+        addresses: [{
+            type:           { type: String }, //TODO enumList
+            address1:       { type: String },
+            address2:       { type: String },
+            city:           { type: String }, //TODO enumlist
+            state:          { type: String }, //TODO enumlist
+            postalCode:     { type: String },
+            country:        { type: String }, //TODO enumlist
+            primary:        { type: Boolean },
+        }],
         referralSource: {
-            primary: {type: String, default: 'Not specified'}
+            primary: {type: String, default: 'Not specified'},
+            specific: { type: String }
+        },
+        status: {
+            current:        { type: Boolean },
+            confidential:   { type: Boolean },  //TODO this shouldn't be part of hiring lifecycle
+            // TODO revise mock data, reconsider with substatus
+            hiringStatus: {
+                submitted: {
+                    date: {type: Date},
+                    note: {type: String}
+                },
+                phoneScreen: {
+                    date: {type: Date},
+                    note: {type: String}
+                },
+                campusVisit: {
+                    date: {type: Date},
+                    note: {type: String}
+                },
+                offered: {
+                    date: {type: Date},
+                    note: {type: String}
+                },
+                hired: {
+                    date: {type: Date},
+                    note: {type: String}
+                },
+                rejected: {
+                    date: {type: Date},
+                    note: {type: String}
+                }
+            }
         },
         dateCreated: {
             type: Date,
