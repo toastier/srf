5aa8e274a6967c49c00039bf2772de93c313aad3
shotokai <david.parrish@duke.edu>
2015-10-03 23:12:14 -0400

    udpating columnDefinition to include sortable: true where appropriate

--- public/modules/applications/controllers/applications.client.controller.js
+++ public/modules/applications/controllers/applications.client.controller.js
@@ -12,7 +12,7 @@
     vm.allowManage = allowManage;
     /** @type ColumnDefinition[] **/
     vm.columnDefinitions = [
-      {field: 'opening.name', label: 'Opening', filterable: true, actions: {
+      {field: 'opening.name', label: 'Opening', sortable: true, filterable: true, actions: {
         restrict: allowView,
         actionItems: [
           {
@@ -31,9 +31,9 @@
           }
         ]
       }},
-      {field: 'applicant.name.honorific', label: 'Hon', filterable: true },
-      {field: 'applicant.name.firstName', label: 'First Name', filterable: true },
-      {field: 'applicant.name.lastName', label: 'Last Name', filterable: true },
+      {field: 'applicant.name.honorific', label: 'Hon', filterable: true, sortable: true },
+      {field: 'applicant.name.firstName', label: 'First Name', filterable: true, sortable: true },
+      {field: 'applicant.name.lastName', label: 'Last Name', filterable: true, sortable: true },
       {field: 'reviewPhase.reviews.reviewer.displayName', label: 'Reviewers', filterable: true, sortable: false}
     ];
 
