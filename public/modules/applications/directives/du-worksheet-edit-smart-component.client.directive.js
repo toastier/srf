6d210520173952e8a4bf6fb3001eaf7a3e075237
shotokai <david.parrish@duke.edu>
2015-10-29 18:22:33 -0400

    [#106968914] As the DFA, I want for review worksheet fields to change when I edit the worksheet fields configuration - added directives to allow display and editing of worksheet fields

---
+++ public/modules/applications/directives/du-worksheet-edit-smart-component.client.directive.js
@@ -0,0 +1,33 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('applications')
+    .directive('duWorksheetEditSmartComponent', duWorksheetEditSmartComponent);
+
+  /* @ngInject */
+  function duWorksheetEditSmartComponent() {
+
+    return {
+      restrict: 'E',
+      templateUrl: 'modules/applications/directives/partials/du-worksheet-edit-smart-component.client.partial.html',
+      controller: duWorksheetEditSmartComponentController,
+      controllerAs: 'vm',
+      bindToController: true,
+      scope: {
+        worksheetFields: '='
+      }
+    };
+
+    function duWorksheetEditSmartComponentController() {
+      var vm = this;
+
+      activate();
+
+      function activate() {
+        // code to run on activation
+      }
+    }
+  }
+})();
+
