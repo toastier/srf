f293b202e57ea0e8ebc6736ae0f80470553c1acc
shotokai <david.parrish@duke.edu>
2015-11-03 13:15:16 -0500

    - fixes error handling in express

--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -1,26 +1,26 @@
 'use strict';
 
-module.exports = function(app) {
-	// Root routing
-	var users = require('../../app/controllers/users.server.controller');
-	var core = require('../../app/controllers/core.server.controller');
+module.exports = function (app) {
+  // Root routing
+  var users = require('../../app/controllers/users.server.controller');
+  var core = require('../../app/controllers/core.server.controller');
 
-	app.route('/').get(core.index);
-	app.route('/app-info').get(core.appInfo);
+  app.route('/').get(core.index);
+  app.route('/app-info').get(core.appInfo);
 
-	function handle403(err, req, res, next) {
-		if (err.status !== 403) {
-			var message = err.message;
-			if (message) {
-				res.send(500, {
-					message: message
-				});
-			}
-			return next(err);
-		}
-		res.send('403 error');
-	}
+  app.use(logErrors);
+  app.use(clientErrorHandler);
 
-	app.use(handle403);
+  function logErrors(err, req, res, next) {
+    console.error(err.stack);
+    next(err);
+  }
 
+  function clientErrorHandler(err, req, res, next) {
+    var message = err.message || 'Internal Server Error';
+    var status = err.status || 500;
+    res.send(status, {
+      message: message
+    });
+  }
 };
