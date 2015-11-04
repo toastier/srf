7b9740bdd94bd69766331495f7938d5faa711fff
shotokai <david.parrish@duke.edu>
2015-11-04 14:13:23 -0500

    adding modal to change the selected Opening

---
+++ public/modules/openings/controllers/change-opening-modal.client.controller.js
@@ -0,0 +1,77 @@
+(function () {
+  'use strict';
+  angular
+    .module('openings')
+    .controller('ChangeOpeningModalController', ChangeOpeningModalController);
+
+  /**
+   * UI-Bootstrap Modal Controller for Selecting an Opening
+   * @description accepts the current Opening Object as currentOpening, and allows the user to
+   * select an different opening from a list.  Returns the newly chosen Opening object
+   * @param $modalInstance
+   * @param Messages
+   * @param Opening
+   * @param currentOpening
+   * @constructor
+   */
+  function ChangeOpeningModalController($modalInstance, Messages, Opening, currentOpening) {
+
+    var vm = this;
+    vm.cancel = cancel;
+    vm.selectedOpening = null;
+    vm.ok = ok;
+    vm.selectOpening = selectOpening;
+
+    activate();
+
+    /**
+     * Get the list of Openings
+     */
+    function activate() {
+      Opening.query().$promise
+        .then(function(openings) {
+          removeCurrentOpeningFromList(openings);
+          vm.openings = openings;
+        })
+        .catch(function (err) {
+          Messages.addMessage(err.data.message, 'error');
+        });
+    }
+
+    /**
+     * Dismiss the Modal and return the reason
+     */
+    function cancel() {
+      $modalInstance.dismiss('User cancelled');
+    }
+
+    /**
+     * Select an Opening from the list
+     * @param opening
+     */
+    function selectOpening(opening) {
+      vm.selectedOpening = opening;
+    }
+
+    /**
+     * Close the Modal and return the selected Opening
+     */
+    function ok() {
+      $modalInstance.close(vm.selectedOpening);
+    }
+
+    /**
+     * Remove the currentOpening (passed as a resolve param in the invoking Controller) from the list.
+     * @param openings
+     */
+    function removeCurrentOpeningFromList(openings) {
+      var matched = false;
+      angular.forEach(openings, function(opening) {
+        if(!matched && currentOpening._id === opening._id) {
+          matched = true;
+          openings.splice(openings.indexOf(opening), 1);
+        }
+      });
+    }
+  }
+})();
