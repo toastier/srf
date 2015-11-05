5ede3e422147c7f89ab6357c51d6a3e3531ebe63
shotokai <david.parrish@duke.edu>
2015-11-05 12:01:20 -0500

    resolves [#105902698] Login button should direct to 'signin' page, so that shib and local uses can use the button

--- public/modules/core/controllers/footer.client.controller.js
+++ public/modules/core/controllers/footer.client.controller.js
@@ -4,9 +4,15 @@
     .module('core')
     .controller('FooterController', FooterController);
 
-    function FooterController (appInfo, resolvedAuth) {
+    function FooterController ($state, appInfo, resolvedAuth) {
       var footer = this;
       footer.user = resolvedAuth;
       footer.appInfo = appInfo.init();
+      footer.signin = signin;
+
+
+      function signin () {
+        $state.go('main.signin');
+      }
     }
 })();
--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -12,10 +12,12 @@
     function activate() {
       if (vm.user._id) {
 
-        if (vm.user.hasRole(['admin'])) {
+        if (vm.user.hasRole(['admin', 'manager'])) {
           $state.go('main.dashboards');
         } else if (vm.user.hasRole(['committee member'])) {
           $state.go('main.dashboards');
+        } else {
+          $state.go('main.listCurrentOpenings');
         }
       } else {
         $state.go('main.listCurrentOpenings');
