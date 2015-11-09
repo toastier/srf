53126a0588154f8d081ebcffa3f9160e32492217
Dave Seidman <david.seidman@duke.edu>
2015-11-09 13:24:45 -0500

    EOE partial refactor to show confirmaton message with optional disability message following EOE submit - (FIXED) Submit button not activated - Brief verbiage after selecting Disability=yes within form - Longer verbiage after submit, following a default paragraph of text that all will see - TODO - include button for Return to Openings

--- public/modules/eoe/directives/du-disability-management-system.client.directive.js
+++ public/modules/eoe/directives/du-disability-management-system.client.directive.js
@@ -3,11 +3,19 @@
   angular
     .module('eoe')
     .directive('duDisabilityManagementSystem', duDisabilityManagementSystem)
+    .directive('duDisabilityBrief', duDisabilityBrief)
+
 
   function duDisabilityManagementSystem() {
     return {
       restrict: 'E',
       templateUrl: 'modules/eoe/directives/partials/du-disability-management-system.client.partial.html'
     };
+  }
+  function duDisabilityBrief() {
+      return {
+        restrict: 'E',
+        templateUrl: 'modules/eoe/directives/partials/du-disability-brief.client.partial.html'
+      };
   }
 })();
