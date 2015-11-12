4aaec0586b168d694146c674467cfc21fc2ad35b
Dave Seidman <david.seidman@duke.edu>
2015-11-12 10:54:01 -0500

    EOE Report - Gender labels and totals displaying in correct order for Ethnicity, Race, Disability - TODO - Veteran x Gender - [resolves #107530110]

--- public/modules/eoe/directives/eoe-report.client.directive.js
+++ public/modules/eoe/directives/eoe-report.client.directive.js
@@ -2,12 +2,20 @@
   'use strict';
   angular
     .module('eoe')
-    .directive('eoeReportGenderHeader', eoeReportGenderHeader);
+      .directive('genderTotals', eoeReportGenderTotals);
+    //.directive('eoeReportGenderHeader', eoeReportGenderHeader);
 
   function eoeReportGenderHeader() {
     return {
       restrict: 'E',
       templateUrl: 'modules/eoe/directives/partials/eoe-report-gender-header.client.partial.html'
+    };
+  }
+
+  function eoeReportGenderTotals() {
+    return {
+      restrict: 'E',
+      templateUrl: 'modules/eoe/directives/partials/eoe-report-gender-totals.client.partial.html'
     };
   }
 })();
