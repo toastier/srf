d4d95ddc103eb355dd74d68a733682691de54ca5
Dave Seidman <david.seidman@duke.edu>
2016-01-10 17:08:01 -0500

    [wip #111240630] Button on ad-hc migration page to trigger conversion of XML to JSON, saves to xml2json file; Next step: extract from xml2json to populate FRS collections

--- config/data/migration/server.js
+++ config/data/migration/server.js
@@ -1,8 +1,32 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    app = express(),
+    fs = require('fs'),
+    url = require('url');
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
+});
+
+
+//app.use(express.static('public'));
+
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.post('/receive', function(request, respond) {
+    var body = '';
+    filePath = __dirname + '/xml2json.json';
+    request.on('data', function(data) {
+        body += data;
+    });
+
+    request.on('end', function (){
+        fs.appendFile(filePath, body, function() {
+            respond.end();
+        });
+    });
 });
 
 var server = app.listen(3000, function () {
@@ -12,25 +36,5 @@ var server = app.listen(3000, function () {
     console.log('Example app listening at http://%s:%s', host, port);
 });
 
-app.use(express.static('public'));
 
-////Lets require/import the HTTP module
-//var http = require('http');
-//
-////Lets define a port we want to listen to
-//const PORT=8080;
-//
-////We need a function which handles requests and send response
-//function handleRequest(request, response){
-//    response.end('It Works!! Path Hit: ' + request.url);
-//}
-//
-////Create a server
-//var server = http.createServer(handleRequest);
-//
-////Lets start our server
-//server.listen(PORT, function(){
-//    //Callback triggered when server is successfully listening. Hurray!
-//    console.log("Server listening on: http://localhost:%s", PORT);
-//});
-//
+
