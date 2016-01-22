855769a4306ed05b3763ef794647b79e31f2ff3c
Dave Seidman <david.seidman@duke.edu>
2016-01-22 12:26:05 -0500

    [wip #111240630] [wip #111240328] All Legacy fields for Applicant available on view and edit forms - Will still transfer data to current fields, but this provides a read-only historical record

--- config/data/migration/public/importLegacyApplicants.js
+++ config/data/migration/public/importLegacyApplicants.js
@@ -12,7 +12,55 @@ function cap1st(string) {    var lower = string.toLowerCase();
 
 db.legacy_tbl_CandidateInformation.find().forEach(function (candidate) {
     db.applicants.insert({
-        "legacy": true,
-        "name": { "firstName" : cap1st(candidate.FName), "lastName" : cap1st(candidate.LName) }
+        "legacy": candidate,
+        "name": { "firstName" : cap1st(candidate.FName),
+            "lastName" : cap1st(candidate.LName)
+        }
     });
+
+
+
+
+
+  /*   applicantPositions: [{
+        positionName: {type: String},
+        dateExpectedCompletion: {type: Date},
+        note: {type: String},
+        institution: {
+            institutionName: {type: String},
+            city: {type: String},
+            state: {type: String},
+            country: {type: String}
+        }
+    }],
+        credentials: [{
+        credential: {type: String},
+        note: {type: String},
+        institution: {type: String},
+        year: {type: Number, min: 1920, max: 2030}
+    }],
+        focalAreas: [{
+        focalArea: {type: String}
+    }],
+        emailAddresses: [{
+        emailAddress: {type: String},
+        primary: {type: Boolean},
+        note: {type: String}
+    }],
+        phoneNumbers: [{
+        phoneNumber: {type: String},
+        type: {type: String},
+        note: {type: String},
+        primary: {type: Boolean}
+    }],
+        addresses: [{
+        type: {type: String}, //TODO enumList
+        address1: {type: String},
+        address2: {type: String},
+        city: {type: String}, //TODO enumlist
+        state: {type: String}, //TODO enumlist
+        postalCode: {type: String},
+        country: {type: String}, //TODO enumlist
+        primary: {type: Boolean}
+    }], */
 })
